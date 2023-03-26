f402fc840575c80a7faed206f7c9dfab1b80ab23
NeneLawani <nene@lawani.com>
2023-03-26 08:47:27 -0700

    Create main.h and _puts.c files

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <unistd.h>
+
+void _puts(char *str);
+
+#endif /* #ifndef MAIN_H */
---
+++ task_0.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+ * _printf - a function that produces output according to a format
+ * @format: character string that directs the format of the output
+ */
+
+int _printf(const char *format, ...);
